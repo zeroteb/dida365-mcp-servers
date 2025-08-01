2172bba75a4ee2b6cc63d44bfde747b7de8a788a
zhanghengyyds <zhanghengyyds@noreply.gitcode.com>
2025-08-01 08:28:38 +0800

    fix 多写重名问题mcp_client_dida365_update_project_by_projectID

--- dist/index.js
+++ dist/index.js
@@ -248,20 +248,6 @@ server.setRequestHandler(ListToolsRequestSchema, async () => {
                     required: ["projectId"],
                 },
             },
-            {
-                name: "update_project_by_projectID",
-                description: "根据projectId删除项目",
-                inputSchema: {
-                    type: "object",
-                    properties: {
-                        projectId: {
-                            type: "string",
-                            description: "项目Id"
-                        }
-                    },
-                    required: ["projectId"],
-                },
-            }
         ],
     };
 });
@@ -410,19 +396,6 @@ server.setRequestHandler(CallToolRequestSchema, async (request) => {
                         {
                             type: "text",
                             text: `项目创建成功: ${JSON.stringify(response.data, null, 2)}`,
-                        },
-                    ],
-                };
-            }
-            case "update_project_by_projectID": {
-                const projectId = args.projectId;
-                throwValidError(projectId, "1");
-                const response = await dida365Api.delete(`/project/${projectId}`);
-                return {
-                    content: [
-                        {
-                            type: "text",
-                            text: `删除项目成功: ${JSON.stringify(response.data, null, 2)}`,
                         },
                     ],
                 };
